853d0bd48d32b17560570ed2873a07069b8492b9
Mark Beswetherick <beswetherickmark@gmail.com>
2020-12-17 17:11:01 -0800

    pushed login and reg

---
+++ code/hg_proj/hg_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+import re
+
+# Create your models here.
+
+class UserManager(models.Manager):
+    def validate(self, postdata):
+        email_check = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        errors = {}
+        if len(postdata['f_n']) < 2:
+            errors['f_n'] = "First name must be 2 or more characters"
+        if len(postdata['l_n']) < 2:
+            errors['l_n'] = "Last name must be 2 or more characters"
+        if not email_check.match(postdata['email']):
+            errors['email'] = "Invalid email address"
+        if len(postdata['pw']) < 8:
+            errors['pw'] = "Password must be at least 8 characters"
+        if postdata['pw'] != postdata['conf_pw']:
+            errors['conf_pw'] = "Password does not match confirm password"
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    
---
+++ code/hg_proj/hg_app/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def login_reg(request):
+    return render(request, "logandreg.html")
+
+def home(request):
+    if 'user_id' not in request.session:
+        return redirect("/")
+    return render(request, "home.html")
+    
+def register(request):
+    if request.method=="POST":
+        errors = User.objects.validate(request.POST)
+        if errors:
+            for error in errors:
+                messages.error(request, errors[error])
+            return redirect('/login_reg')
+
+        # password encrypt
+        user_pw = request.POST['pw']
+        hash_pw = bcrypt.hashpw(user_pw.encode(), bcrypt.gensalt()).decode()
+
+        # create the new user
+        new_user = User.objects.create(first_name=request.POST['f_n'], last_name=request.POST['l_n'], email=request.POST['email'], password=hash_pw)
+        print(f"first_name:{request.POST['f_n']}, last_name:{request.POST['l_n']}, email:{request.POST['email']}, password:{request.POST['pw']}, (hash: {hash_pw}).")
+
+        # store user info in session
+        request.session['user_id'] = new_user.id
+        request.session['user_name'] = f"{new_user.first_name} {new_user.last_name}"
+
+        return redirect('/home')
+
+    # was not a post request, send user back to home page
+    return redirect('/')
+
+def login(request):
+    if request.method == 'POST':
+        # see if email is in the DB
+        logged_user = User.objects.filter(email=request.POST['email'])
+        if logged_user:
+            logged_user = logged_user[0]    # strip the curlies 
+            # compare the passwords
+            if bcrypt.checkpw(request.POST['pw'].encode(), logged_user.password.encode()):
+                request.session['user_id'] = logged_user.id
+                request.session['user_name'] = f"{logged_user.first_name} {logged_user.last_name}"
+                return redirect('/home')
+            else:
+                # bad password
+                messages.error(request, "Incorrect password")
+                return redirect('/login_reg')
+        else: 
+            # didn't find user in the database
+            messages.error(request, "Email address is not registered")
+            return redirect('/login_reg')
+
+    # was not a post request, send user back to home page
+    return redirect('/')
